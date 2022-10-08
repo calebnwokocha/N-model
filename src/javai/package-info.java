e39352d32837aed2a77290579bc814427372f1bb
Caleb P. Nwokocha <nwo20001@byui.edu>
2022-10-08 16:08:22 -0500

    testing

--- src/javai/package-info.java
+++ src/javai/package-info.java
@@ -1,6 +1,8 @@
 /**
  * Open-source framework for comprehensive learning.
+ * <p>
  * Available under GNU Affero General Public License.
+ * <p>
  * Email info@javai.org for IT support or any other inquiry.
  */
 package javai;
