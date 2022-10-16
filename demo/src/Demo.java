0236ece5afdfd84665a6444a14275bc65bb5df84
Caleb P. Nwokocha <nwo20001@byui.edu>
2022-10-16 00:34:46 -0500

    testing

--- demo/src/Demo.java
+++ demo/src/Demo.java
@@ -23,9 +23,6 @@
 
 import javai.Dataset;
 import javai.Layer;
-import javai.Network;
-
-import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
@@ -85,40 +82,92 @@ public class Demo {
 
         Dataset dataset1 = new Dataset(fileNames1);
         Dataset dataset2 = new Dataset(fileNames2);
+        Dataset dataset3 = new Dataset();
+
+
+        double[] x1 = new double[100];
+        double[] x2 = new double[100];
+        int[] y = new int[100];
+        Double[][] dset = new Double[100][2];
+        for (int i = 0; i < 100; i++) {
+            x1[i] = Math.random();
+            x2[i] = Math.random();
+            y[i] = (int) Math.round((x1[i] + x2[i]) - (2 * x1[i] + x2[i]));
+        }
+
+        for (int i = 0; i < 100; i++) {
+            dset[i][0] = x1[i];
+            dset[i][1] = x2[i];
+        }
+
+        dataset3.setDataset(dset);
+
+
+        Layer[] layer = new Layer[10];
+        for (int i = 0; i < layer.length; i++) {
+            if (i == layer.length - 1) {
+                layer[i] = new Layer(1);
+            } else { layer[i] = new Layer(1); }
+            layer[i].setFunctionName("sum");
+            layer[i].setPower(1.0);
+            layer[i].setCoverage(7.0);
+        }
+
+        // Train
+        for (int i = 0; i < dataset3.getDataset().length; i++) {
+            for (int j = 0; j < layer.length; j++) {
+                if (j == 0) {
+                    layer[j].train(i + 1, new double[]{y[i]}, dataset3.getDataset()[i]);
+                } else {
+                    layer[j].train(i + 1, new double[]{y[i]}, layer[j - 1].getThesisVec());
+                }
+            }
+
+            /*System.out.println("Example " + i);
+            System.out.println("Input is " + Arrays.toString(dataset3.getDataset()[i]));
+            System.out.println("Objective is " + y[i]);
+            System.out.println("Network prediction is " + Arrays.toString(layer[layer.length - 1].getThesisVec()));
+            System.out.println("Network error is " + Arrays.toString(layer[layer.length - 1].getErrorMeanVec()));
+            System.out.println();*/
+
+            System.out.println(layer[layer.length - 1].getErrorMeanVec()[0]);
+        }
 
 /*        dataset2 = new Dataset();
         dataset2.setDataset(new Double[][] {
                 {1143438888888.0, 234358888888883.0, 2348888888888231.5, 1343.0, 1354153.12}
         });*/
 
-        Network network = new Network(5, 10);
-        network.setFunctionName("sum");
-        network.setPower(1.0);
-        network.setCoverage(7.0);
+/*        Network network1 = new Network(5, 10);
+        network1.setFunctionName("sum");
+        network1.setPower(1.0);
+        network1.setCoverage(7.0);
 
         // Train
         for (int i = 0; i < dataset1.getDataset().length; i++) {
             System.out.println("Example " + i);
-            network.train(i + 1, new double[]{100000, 10, 100, 10, 100}, dataset1.getDataset()[i]);
-            for (Layer layer : network.getLayers()) {
+            network1.train(i + 1, new double[]{100000, 10, 100, 10, 100}, dataset1.getDataset()[i]);
+            for (Layer layer : network1.getLayers()) {
                 System.out.println(Arrays.toString(layer.getThesisVec()));
                 System.out.println();
             }
-        }
+        }*/
 
-        //network.train(1, new double[]{-100000, 10, 100, 10, 100}, dataset1.getDataset());
+        //network1.train(1, new double[]{-100000, 10, 100, 10, 100}, dataset1.getDataset());
 
 
         // Test
+/*
         dataset1.shuffle();
         for (int i = 0; i < dataset2.getDataset().length; i++) {
             System.out.println("Test " + i);
-            network.test(dataset2.getDataset()[i]);
-            System.out.println(Arrays.toString(network.getLayers().get(network.getLayers().size() - 1).getThesisVec()));
+            network1.test(dataset2.getDataset()[i]);
+            System.out.println(Arrays.toString(network1.getLayers().get(network1.getLayers().size() - 1).getThesisVec()));
             System.out.println();
         }
+*/
 
-        //network.test(dataset1.getDataset());
-        //System.out.println(Arrays.toString(network.getLayers().get(network.getLayers().size() - 1).getThesisVec()));
+        //network1.test(dataset1.getDataset());
+        //System.out.println(Arrays.toString(network1.getLayers().get(network1.getLayers().size() - 1).getThesisVec()));
     }
 }
--- src/javai/CFunction.java
+++ src/javai/CFunction.java
@@ -64,6 +64,6 @@ public class CFunction {
 
     private void sum (Double... X) {
         double s = 0;
-        for (double x : X) { s += x; } this.value =  Math.pow(s, -0.8);
+        for (double x : X) { s += x; } this.value =  s;
     }
 }
