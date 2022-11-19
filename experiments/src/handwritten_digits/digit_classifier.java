67ed5332c6896cc0f92fce55b8d693e287923f67
Caleb Princewill Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2022-11-19 11:29:57 -0600

    testing

--- experiments/src/handwritten_digits/digit_classifier.java
+++ experiments/src/handwritten_digits/digit_classifier.java
@@ -5,7 +5,7 @@ import cml.Dataset;
 import java.io.IOException;
 
 public class digit_classifier {
-    Dataset ZeroDataset = new Dataset("handwritten_digits/dataset/250000_Final/0");
+    Dataset ZeroDataset = new Dataset("handwritten_digits/dataset/10000/0");
 
     public digit_classifier() throws IOException {
     }
