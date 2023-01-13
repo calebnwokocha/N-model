433c5ee9591dfe6e866308787cc7ce072f707cc4
Caleb P. Nwokocha <nwo20001@byui.edu>
2023-01-13 16:00:00 -0600

    testing

--- experiments/src/logistic_regression.java
+++ experiments/src/logistic_regression.java
@@ -11,61 +11,76 @@ import java.util.function.Function;
 public class logistic_regression {
     public static void main(String[] args) throws Exception {
         Multitask multitask = new Multitask();
-        Network[] network = new Network[10];
-        Double[][] networkObjective = new Double[10][];
-        Double[][][] trainSet = new Double[10][8][1];
-        Double[][][] testSet = new Double[10][20][1];
+        Network[] networks = new Network[10];
+        Double[][] networkObjectives = new Double[10][];
+        Double[][][] trainSets = new Double[10][8][1];
+        Double[][][] testSets = new Double[10][20][1];
 
-        for (int i = 1; i < networkObjective.length; i++) {
-            networkObjective[i] = new Double[]{(double) (i % 2)};
+        for (int i = 1; i < networkObjectives.length; i++) {
+            networkObjectives[i] = new Double[]{(double) (i % 2)};
         }
 
-        for (int i = 1; i < trainSet.length; i++){
-            for (int j = 0; j < trainSet[i].length; j++) {
-                trainSet[i][j][0] = Math.random() + i;
+        for (int i = 1; i < trainSets.length; i++){
+            for (int j = 0; j < trainSets[i].length; j++) {
+                trainSets[i][j][0] = Math.random() + i;
             }
         }
 
-        for (int i = 1; i < testSet.length; i++){
-            for (int j = 0; j < testSet[i].length; j++) {
-                testSet[i][j][0] = Math.random() + i;
+        for (int i = 1; i < testSets.length; i++){
+            for (int j = 0; j < testSets[i].length; j++) {
+                testSets[i][j][0] = Math.random() + i;
             }
         }
 
-        Function<Double[], Double> sum = x -> {
-            Double s = 0.0;
-            for (Double d : x) { s += d; }
-            return s;
-        };
+        Function<Double[], Double> square = x -> Math.pow(x[0], 2.0);
 
-        for (int i = 1; i < network.length; i++){
-            network[i] = new Network(2, 1);
-            network[i].setCFunction("sum", 1.0, sum);
-            network[i].setPower(-6.0);
-            network[i].setCoverage(4.0);
+        for (int i = 1; i < networks.length; i++){
+            networks[i] = new Network(2, 1);
+            networks[i].setCFunction("sum", 2.0, square);
+            networks[i].setPower(-6.0);
+            networks[i].setCoverage(4.0);
         }
 
-        System.out.println("\nNETWORK TRAINING....................................................................");
+        System.out.println("TRAINING ERROR MEAN.................................................................");
+        for (int i = 1; i < networks.length; i++) {
+            multitask.addNetwork(networks[i]);
+            System.out.println("\nTask " + i);
+            for (int j = 0; j < trainSets[i].length; j++) {
+                multitask.train(j + 1, networkObjectives[i], trainSets[i][j]);
+                System.out.println(multitask.getNetworks().get(0).getErrorMean()[networks[i].getLength() - 1][0]);
+            }
+        }
+
+        System.out.println("\n\nTESTING ERROR MEAN.................................................................");
+        for (int i = 1; i < networks.length; i++) {
+            System.out.println("\nTask " + i);
+            for (int j = 0; j < testSets[i].length; j++) {
+                multitask.test(testSets[i][j]);
+                System.out.println(multitask.getNetworks().get(0).getErrorMean()[networks[i].getLength() - 1][0]);
+            }
+        }
+
+        /*System.out.println("\nNETWORK TRAINING....................................................................");
         System.out.println();
         System.out.println();
         int k = 0;
-        for (int i = 1; i < network.length; i++) {
-            multitask.addNetwork(network[i]);
-            for (int j = 0; j < trainSet[i].length; j++, k++) {
-                multitask.train(j + 1, networkObjective[i], trainSet[i][j]);
+        for (int i = 1; i < networks.length; i++) {
+            multitask.addNetwork(networks[i]);
+            for (int j = 0; j < trainSets[i].length; j++, k++) {
+                multitask.train(j + 1, networkObjectives[i], trainSets[i][j]);
                 System.out.println("Example " + (k + 1) + ":");
                 System.out.println();
-                System.out.println("Network objective is " + Arrays.toString(networkObjective[i]));
+                System.out.println("Network objective is " + Arrays.toString(networkObjectives[i]));
                 System.out.println();
-                System.out.println("Network input is " + Arrays.toString(trainSet[i][j]));
+                System.out.println("Network input is " + Arrays.toString(trainSets[i][j]));
                 System.out.println();
                 System.out.println("Network hypothesis is " + Arrays.toString(multitask.getNetworks().get(0).
-                        getHypothesis()[network[i].getLength() - 1]));
+                        getHypothesis()[networks[i].getLength() - 1]));
                 System.out.println();
                 System.out.println("Network thesis is " + Arrays.deepToString(multitask.getThesis()));
                 System.out.println();
                 System.out.println("Network error mean is " + Arrays.toString(multitask.getNetworks().get(0)
-                        .getErrorMean()[network[i].getLength() - 1]));
+                        .getErrorMean()[networks[i].getLength() - 1]));
                 System.out.println();
             }
         }
@@ -76,19 +91,19 @@ public class logistic_regression {
         System.out.println();
         System.out.println();
         k = 0;
-        for (int i = 1; i < network.length; i++) {
-            for (int j = 0; j < testSet[i].length; j++, k++) {
-                multitask.test(testSet[i][j]);
+        for (int i = 1; i < networks.length; i++) {
+            for (int j = 0; j < testSets[i].length; j++, k++) {
+                multitask.test(testSets[i][j]);
                 System.out.println("Test " + (k + 1) + ":");
                 System.out.println();
-                System.out.println("Network objective is " + Arrays.toString(networkObjective[i]));
+                System.out.println("Network objective is " + Arrays.toString(networkObjectives[i]));
                 System.out.println();
-                System.out.println("Network input is " + Arrays.toString(testSet[i][j]));
+                System.out.println("Network input is " + Arrays.toString(testSets[i][j]));
                 System.out.println();
                 System.out.println("Network thesis is " + Arrays.deepToString(multitask.getThesis()));
                 System.out.println();
                 System.out.println();
             }
-        }
+        }*/
     }
 }
