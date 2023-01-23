8e363bb5b4f4becda690f9559c6be43e7d4bd7e6
Caleb P. Nwokocha <nwo20001@byui.edu>
2023-01-22 21:29:02 -0600

    documentation

--- src/model/Multitask.java
+++ src/model/Multitask.java
@@ -8,29 +8,65 @@ package model;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Multitask class creates an object that holds multiple instances of the network class
+ * and can train or test them at once. It also allows for knowledge transfer between
+ * the networks and keeps track of the null frequency of each network.
+ */
 public class Multitask {
     private ArrayList<Network> networks = new ArrayList<>();
     private Double[][] errorMeanMean;
     private int lastNetworkIndex;
     private final ArrayList<Integer> nullFrequencyPerNetwork = new ArrayList<>();
 
+    /**
+     * Creates an empty instance of the multitask class
+     */
     public Multitask () {}
 
+    /**
+     * Creates an instance of the multitask class with the specified networks
+     * @param networks the networks to be included in the multitask object
+     */
     public Multitask (ArrayList<Network> networks) { this.networks = networks; }
 
+    /**
+     * Returns the number of networks in the multitask object
+     * @return the number of networks
+     */
     public int getTaskCount() { return networks.size(); }
 
+    /**
+     * Sets the networks in the multitask object
+     * @param networks the networks to be set in the multitask object
+     */
     public void setNetworks(ArrayList<Network> networks) { this.networks = networks; }
 
+    /**
+     * Returns the networks in the multitask object
+     * @return the networks in the multitask object
+     */
     public ArrayList<Network> getNetworks() { return this.networks; }
 
+    /**
+     * Adds a new network to the multitask object and transfers knowledge from the previous network
+     * @param network the new network to be added
+     */
     public void addNetwork (Network network) {
         if (networks.size() >= 1) { this.transferKnowledgeTo(network); }
         this.networks.add(network);
     }
 
+    /**
+     * Removes a network at the specified index from the multitask object
+     * @param index the index of the network to be removed
+     */
     public void removeNetwork (int index) { this.networks.remove(index); }
 
+    /**
+     * Returns the coverage of all networks in the multitask object as a 3D array
+     * @return the coverage of all networks
+     */
     public Double[][][] getCoverage () {
         Double[][][] coverage = new Double[this.networks.size()][][];
         for (int i = 0; i < coverage.length; i++) {
@@ -38,6 +74,10 @@ public class Multitask {
         } return coverage;
     }
 
+    /**
+     * Returns the thesis of all networks in the multitask object as a 2D array
+     * @return the thesis of all networks
+     */
     public Double[][] getThesis () {
         Double[][] thesis = new Double[this.networks.size()][];
         for (int i = 0; i < thesis.length; i++) {
@@ -47,11 +87,25 @@ public class Multitask {
         } return thesis;
     }
 
+    /**
+     * The train method is used to train the last network added to the multitask network.
+     *
+     * @param iteration The number of iterations the network should be trained for.
+     * @param objective The expected output for the given input.
+     * @param input The input to be trained on.
+     */
     public void train (int iteration, Double[] objective, Double[]... input) {
         this.lastNetworkIndex = this.networks.size() - 1;
         this.networks.get(this.lastNetworkIndex).train(iteration, objective, input);
     }
 
+    /**
+     * The test method is used to test all the networks in the multitask network.
+     * It also prints the null frequency per network of the multitask network.
+     *
+     * @param iteration The number of iterations the network should be tested for.
+     * @param input The input to be tested on.
+     */
     public void test (int iteration, Double[]... input) { for (Network network : this.networks) { network.test(input); }
         Double[][] thesis = this.getThesis();
         int[][] nullNonNullAs10 = this.convertNullTo1AndNonNullTo0(thesis);
@@ -61,12 +115,17 @@ public class Multitask {
         } else { for (int i = 0; i < nullCountPerNetwork.length; i++) {
             this.nullFrequencyPerNetwork.set(i, this.nullFrequencyPerNetwork.get(i) + nullCountPerNetwork[i]);
         }
-        } System.out.println("Null frequence per network of multitask: "
+        } System.out.println("Non-null thesis frequence per network of multitask: "
                 + Arrays.toString(new ArrayList[]{this.nullFrequencyPerNetwork}));
         System.out.println();
         System.out.println();
     }
 
+    /**
+     * The transferKnowledgeTo method is used to transfer knowledge from the last network to the current network.
+     *
+     * @param network The current network that knowledge is being transferred to.
+     */
     private void transferKnowledgeTo(Network network) {
         if (networks.size() > 0) {
             this.lastNetworkIndex = this.networks.size() - 1;
@@ -80,27 +139,45 @@ public class Multitask {
         }
     }
 
+    /**
+     * The hasOnlyOneNetwork method checks if the multitask network has only one network.
+     *
+     * @return A boolean indicating if the multitask network has only one network.
+     */
     private boolean hasOnlyOneNetwork () { return this.networks.size() == 1; }
 
+    /**
+     * convertNullTo1AndNonNullTo0 is a helper function that converts all the null values in a 2D Double array to 0
+     * and all non-null values to 1.
+     *
+     * @param thesis a 2D Double array containing the thesis of the network
+     * @return an int[][] where all the null values in the input thesis are converted to 0 and all non-null values to 1.
+     */
     private int[][] convertNullTo1AndNonNullTo0 (Double[][] thesis) {
-        int[][] nullNonNullAs10 = new int[thesis.length][];
+        int[][] nullNonNullAs01 = new int[thesis.length][];
         for (int i = 0; i < thesis.length; i++) {
-            nullNonNullAs10[i] = new int[thesis[i].length];
+            nullNonNullAs01[i] = new int[thesis[i].length];
             for (int j = 0; j < thesis[i].length; j++) {
-                if (thesis[i][j] == null) { nullNonNullAs10[i][j] = 1; }
-                else { nullNonNullAs10[i][j] = 0; }
+                if (thesis[i][j] == null) { nullNonNullAs01[i][j] = 0; }
+                else { nullNonNullAs01[i][j] = 1; }
             }
-        } return nullNonNullAs10;
+        } return nullNonNullAs01;
     }
 
-    private int[] countNullsPerNetwork (int[][] nullNonNullAs10) {
-        int[] nullCountPerNetwork = new int[nullNonNullAs10.length];
-        Arrays.fill(nullCountPerNetwork, 0);
-        for (int i = 0; i < nullCountPerNetwork.length; i++) {
-            for (int j = 0; j < nullNonNullAs10[i].length; j++) {
-                nullCountPerNetwork[i] += nullNonNullAs10[i][j];
+    /**
+     * countNullsPerNetwork is a helper function that counts the number of non-null values in each network
+     *
+     * @param nullNonNullAs01 a 2D int array where all the null values are converted to 0 and all non-null values to 1
+     * @return an int[] where the i-th element represents the number of non-null values in the i-th network
+     */
+    private int[] countNullsPerNetwork (int[][] nullNonNullAs01) {
+        int[] nonnullCountPerNetwork = new int[nullNonNullAs01.length];
+        Arrays.fill(nonnullCountPerNetwork, 0);
+        for (int i = 0; i < nonnullCountPerNetwork.length; i++) {
+            for (int j = 0; j < nullNonNullAs01[i].length; j++) {
+                nonnullCountPerNetwork[i] += nullNonNullAs01[i][j];
             }
-        } return nullCountPerNetwork;
+        } return nonnullCountPerNetwork;
     }
 
     private void forgetNetwork () {
--- src/model/Network.java
+++ src/model/Network.java
@@ -8,36 +8,92 @@ package model;
 import java.util.ArrayList;
 import java.util.function.Function;
 
+/**
+ The Network class represents a comprehensive network and provides methods for creating and
+ manipulating the network. The class has a private field called 'layers' which is an
+ ArrayList of Layer objects. Users can create a new network by specifying the number
+ of layers and the width of each layer. The class also provides methods for getting
+ and setting the layers in the network, adding and removing layers, getting the number
+ of layers, getting the width of each layer, and setting the comprehensive function for all
+ layers in the network. The class also provides methods for setting the
+ degree for each layer in the layers list.
+ */
 public class Network {
     private ArrayList<Layer> layers = new ArrayList<>();
 
+    /**
+     * Creates a new network with the specified number of layers and a width of each layer.
+     * @param length The number of layers in the network
+     * @param width The width of each layer in the network
+     */
     public Network (int length, int width) {
         for (int i = 0; i < length; i++) { this.layers.add(new Layer(width)); }
     }
 
+    /**
+     * Creates a new network with the specified number of layers and a width of each layer.
+     * @param length The number of layers in the network
+     * @param width The width of each layer in the network as an array
+     */
     public Network (int length, int[] width) {
         for (int i = 0; i < length; i++) { this.layers.add(new Layer(width[i])); }
     }
 
+    /**
+     * Returns the layers in the network.
+     * @return An ArrayList of the layers in the network
+     */
     public ArrayList<Layer> getLayers () { return this.layers; }
 
+    /**
+     * Sets the layers in the network.
+     * @param layers An ArrayList of layers to be set in the network
+     */
     public void setLayers (ArrayList<Layer> layers) { this.layers = layers; }
 
+    /**
+     * Adds a new layer to the network.
+     * @param layer The new layer to be added to the network
+     */
     public void addLayer (Layer layer) { this.layers.add(layer); }
 
+    /**
+     * Removes a layer at the specified index from the network.
+     * @param index The index of the layer to be removed
+     */
     public void removeLayer (int index) { this.layers.remove(index); }
 
+    /**
+     * Returns the number of layers in the network.
+     * @return The number of layers in the network
+     */
     public int getLength () { return this.layers.size(); }
 
+    /**
+     * Returns the width of each layer in the network as an array.
+     * @return An array of the width of each layer in the network
+     */
     public int[] getWidth () { int[] width = new int[this.layers.size()];
         for (int i = 0; i < width.length; i++) { width[i] = this.layers.get(i).getWidth(); }
         return width;
     }
 
+    /**
+     * Sets the cost function for all layers in the network.
+     * @param cFunctionName The name of the comprehensive function
+     * @param degree The degree of the comprehensive function
+     * @param cFunction The cost function to be set
+     */
     public void setCFunction (String cFunctionName, Double degree, Function<Double[], Double> cFunction) {
         for (Layer layer : layers) { layer.setCFunction (cFunctionName, degree, cFunction); }
     }
 
+    /**
+     * This method sets the cFunction, degree, and cFunctionName for each layer in the layers list.
+     * @param cFunctionName an array of strings representing the names of the cFunctions for each layer
+     * @param degree an array of doubles representing the degrees of the cFunctions for each layer
+     * @param cFunction an array of functions that take in an array of doubles and return a double
+     */
     public void setCFunction(String[] cFunctionName, Double[] degree,
                              Function<Double[], Double>[] cFunction) {
         for (int i = 0; i < layers.size(); i++) {
@@ -45,6 +101,12 @@ public class Network {
         }
     }
 
+    /**
+     * This method sets the cFunction, degree, and cFunctionName for each layer in the layers list.
+     * @param cFunctionName a 2D array of strings representing the names of the cFunctions for each layer
+     * @param degree a 2D array of doubles representing the degrees of the cFunctions for each layer
+     * @param cFunction a 2D array of functions that take in an array of doubles and return a double
+     */
     public void setCFunction(String[][] cFunctionName, Double[][] degree,
                              Function<Double[], Double>[][] cFunction) {
         for (int i = 0; i < layers.size(); i++) {
@@ -52,71 +114,141 @@ public class Network {
         }
     }
 
+    /**
+     * This method returns a 2D array of strings representing the names of the cFunctions for each layer
+     * @return a 2D array of strings representing the names of the cFunctions for each layer
+     */
     public String[][] getCFunctionName() { String[][] cFunction = new String[this.layers.size()][];
-        for (int i = 0; i < cFunction.length; i++) {
-            cFunction[i] = layers.get(i).getCFunctionName();
-        } return cFunction;
+        for (int i = 0; i < cFunction.length; i++) { cFunction[i] = layers.get(i).getCFunctionName(); }
+        return cFunction;
     }
 
+    /**
+     * This method returns a 2D array of doubles representing the degrees of the cFunctions for each layer
+     * @return a 2D array of doubles representing the degrees of the cFunctions for each layer
+     */
     public Double[][] getDegree() {
         Double[][] degree = new Double[this.layers.size()][];
         for (int i = 0; i < degree.length; i++) { degree[i] = layers.get(i).getDegree(); }
         return degree;
     }
 
+    /**
+     * This method sets the power for each layer in the layers list.
+     * @param power a double representing the power for each layer
+     */
     public void setPower (Double power) { for (Layer layer : this.layers) { layer.setPower(power); } }
 
+    /**
+     * This method sets the power for each layer in the layers list.
+     * @param power an array of doubles representing the power for each layer
+     */
     public void setPower (Double[] power) {
         for (Layer layer : this.layers) { layer.setPower(power); }
     }
 
-    public void setPower (Double[][] power) {
+    /**
+     * Set the power of each layer
+     *
+     * @param power a 2D array of Double representing the power for each layer and each node
+     */
+    public void setPower(Double[][] power) {
         for (int i = 0; i < layers.size(); i++) { layers.get(i).setPower(power[i]); }
     }
 
-    public Double[][] getPower () {
+    /**
+     * Get the power of each layer
+     *
+     * @return a 2D array of Double representing the power for each layer and each node
+     */
+    public Double[][] getPower() {
         Double[][] power = new Double[this.layers.size()][];
         for (int i = 0; i < power.length; i++) { power[i] = layers.get(i).getPower(); }
         return power;
     }
 
-    public void setCoverage (Double coverage) {
+    /**
+     * Set the coverage of each layer
+     *
+     * @param coverage a double value representing the coverage for all layers
+     */
+    public void setCoverage(Double coverage) {
         for (Layer layer : this.layers) { layer.setCoverage(coverage); }
     }
 
-    public void setCoverage (Double[] coverage) { for (Layer layer : this.layers) { layer.setCoverage(coverage); } }
+    /**
+     * Set the coverage of each layer
+     *
+     * @param coverage an array of double value representing the coverage for each layer
+     */
+    public void setCoverage(Double[] coverage) { for (Layer layer : this.layers) { layer.setCoverage(coverage); } }
 
-    public void setCoverage (Double[][] coverage) {
+    /**
+     * Set the coverage of each layer
+     *
+     * @param coverage a 2D array of double value representing the coverage for each layer and each node
+     */
+    public void setCoverage(Double[][] coverage) {
         for (int i = 0; i < layers.size(); i++) { layers.get(i).setCoverage(coverage[i]); }
     }
 
-    public Double[][] getCoverage () {
+    /**
+     * Get the coverage of each layer
+     *
+     * @return a 2D array of Double representing the coverage for each layer and each node
+     */
+    public Double[][] getCoverage() {
         Double[][] coverage = new Double[this.layers.size()][];
         for (int i = 0; i < coverage.length; i++) { coverage[i] = layers.get(i).getCoverage(); }
         return coverage;
     }
 
-    public Double[][] getErrorMean() { Double[][] errorMean = new Double[this.layers.size()][];
-        for (int i = 0; i < errorMean.length; i++) {
-            errorMean[i] = this.layers.get(i).getErrorMean();
-        } return errorMean;
+    /**
+     * Get the mean error of each layer
+     *
+     * @return a 2D array of Double representing the error mean for each layer and each node
+     */
+    public Double[][] getErrorMean() {
+        Double[][] errorMean = new Double[this.layers.size()][];
+        for (int i = 0; i < errorMean.length; i++) { errorMean[i] = this.layers.get(i).getErrorMean(); }
+        return errorMean;
     }
 
-    public void setErrorMean (Double[][] errorMean) {
+    /**
+     * Set the mean error of each layer
+     *
+     * @param errorMean a 2D array of Double representing the error mean for each layer and each node
+     */
+    public void setErrorMean(Double[][] errorMean) {
         for (int i = 0; i < layers.size(); i++) { layers.get(i).setErrorMean(errorMean[i]); }
     }
 
-    public Double[][] getHypothesis() { Double[][] hypothesis = new Double[this.layers.size()][];
-        for (int i = 0; i < hypothesis.length; i++) {
-            hypothesis[i] = this.layers.get(i).getHypothesis();
-        } return hypothesis;
+    /**
+     * Returns the hypothesis of all the layers in the network
+     * @return Double[][] hypothesis
+     */
+    public Double[][] getHypothesis() {
+        Double[][] hypothesis = new Double[this.layers.size()][];
+        for (int i = 0; i < hypothesis.length; i++) { hypothesis[i] = this.layers.get(i).getHypothesis(); }
+        return hypothesis;
     }
 
-    public Double[][] getThesis() { Double[][] thesis = new Double[this.layers.size()][];
+    /**
+     * Returns the thesis of all the layers in the network
+     * @return Double[][] thesis
+     */
+    public Double[][] getThesis() {
+        Double[][] thesis = new Double[this.layers.size()][];
         for (int i = 0; i < thesis.length; i++) { thesis[i] = this.layers.get(i).getThesis(); }
         return thesis;
     }
 
+    /**
+     * Trains the network using the input and objective provided
+     * @param iteration the number of iterations to train the network
+     * @param objective the objective/expected output of the network
+     * @param input the input to the network
+     */
     public void train (int iteration, Double[] objective, Double[]... input) {
         for (int i = 0; i < this.layers.size(); i++) {
             if (i == 0) { this.layers.get(i).train(iteration, objective, input); }
@@ -126,22 +258,44 @@ public class Network {
         }
     }
 
+    /**
+     * Test the network using the given input.
+     * @param input The input values to test the network with.
+     */
     public void test (Double[]... input) {
         for (int i = 0; i < this.layers.size(); i++) {
-            if (i == 0) {this.layers.get(i).test(input);
-                if (this.layers.get(i).isNull()) { this.nullifyNetOutput(); }
-            } else { Double[] previousLayerThesis = this.layers.get(i - 1).getThesis();
+            if (i == 0) {
+                // If this is the first layer, pass the input directly to the test method
+                this.layers.get(i).test(input);
+                if (this.layers.get(i).isNull()) {
+                    // If the output is null, nullify the entire network's output
+                    this.nullifyNetOutput();
+                }
+            } else {
+                // If this is not the first layer, pass the previous layer's thesis as input
+                Double[] previousLayerThesis = this.layers.get(i - 1).getThesis();
                 this.layers.get(i).test(this.vectorToMatrix(previousLayerThesis));
-                if (this.layers.get(i).isNull()) { this.nullifyNetOutput(); }
+                if (this.layers.get(i).isNull()) {
+                    // If the output is null, nullify the entire network's output
+                    this.nullifyNetOutput();
+                }
             }
         }
     }
 
+    /**
+     * Utility function to convert a vector to a matrix
+     * @param vector The vector to convert
+     * @return matrix The converted matrix
+     */
     private Double[][] vectorToMatrix(Double[] vector) {
         Double[][] matrix = new Double[vector.length][1];
         for (int i = 0; i < matrix.length; i++) { matrix[i][0] = vector[i]; }
         return matrix;
     }
 
+    /**
+     * Set the output of the last layer to null
+     */
     private void nullifyNetOutput () { this.layers.get(this.getLength() - 1).test(null); }
 }
